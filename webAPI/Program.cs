84b05035e88ec1c4fbe309061ea37f4e39726ae6
Bri Achtman <brachtma@microsoft.com>
2021-02-05 14:40:42 -0800

    Initial commit

---
+++ webAPI/Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+
+namespace MBGeneratedCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configuration
+            WebHost.CreateDefaultBuilder()
+                .ConfigureServices(services => {
+                    // Register SentimentModel
+                    services.AddSentimentModel();
+                })
+                .Configure(options => {
+                    options.UseRouting();
+                    options.UseEndpoints(routes => {
+                        // Define prediction endpoint
+                        routes.MapPost("/predict", PredictHandler);
+                    });
+                })
+                .Build()
+                .Run();
+        }
+
+        static async Task PredictHandler(HttpContext http)
+        {
+            // Get PredictionEnginePool service
+            var sentimentModel = http.RequestServices.GetRequiredService<SentimentModel>();
+
+            // Deserialize HTTP request JSON body
+            var body = http.Request.Body as Stream;
+            var input = await JsonSerializer.DeserializeAsync<SentimentModel.ModelInput>(body);
+
+            // Predict
+            var prediction = sentimentModel.Predict(input);
+
+            // Return prediction as response
+            await http.Response.WriteAsJsonAsync(prediction);
+        }
+    }
+}
