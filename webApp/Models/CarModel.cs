9acaca4070af27f6300fe06632000268bac2d5c1
Bri Achtman <brachtma@microsoft.com>
2021-02-08 13:32:36 -0800

    add web api consumption code

---
+++ webApp/Models/CarModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApp.Models
+{
+    public class CarModel
+    {
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ webApp/Pages/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace webApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task OnGet()
+        {
+          var request = new HttpRequestMessage(HttpMethod.Post,
+          "http://localhost:57311/predict");
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Branches = await JsonSerializer.DeserializeAsync
+                    <IEnumerable<GitHubBranch>>(responseStream);
+            }
+            else
+            {
+                GetBranchesError = true;
+                Branches = Array.Empty<GitHubBranch>();
+            }
+        }
+    }
+
+    public class ModelOutput
+    {
+        public string Prediction { get; set; }
+        public float[] Score { get; set; }
+    }
+}
